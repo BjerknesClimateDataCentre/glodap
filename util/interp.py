f9dfa2c9cbf90680db8d3f20ef8452bd19525b14
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-04-11 09:35:25 +0200

    (#11) Function to fill profile gaps with nans

--- util/interp.py
+++ util/interp.py
@@ -52,7 +52,8 @@ def pchip_interpolate_profile(
 ):
     """
     Interpolates all variables in the data with respect to the dimension_key -
-    variable.
+    variable. Keeps the original data, and tries to align this with the
+    interpolated values.
     """
     # Make sure data are sorted and monotonic
     data = average_values_for_duplicate_dimension(data, dimension_key)
@@ -73,6 +74,10 @@ def pchip_interpolate_profile(
     if len(dimension)<len(data):
         raise Exception(
             'Interpolated values cannot be less than actual data points'
+        )
+    if len(data)<2:
+        raise Exception(
+            'Needs at least 2 data points to interpolate'
         )
 
     output[dimension_key + suffix] = dimension
@@ -97,3 +102,57 @@ def pchip_interpolate_profile(
         prev = i
 
     return output
+
+def subst_depth_profile_gaps_with_nans (
+        data: pd.DataFrame,
+        dimension_key: str,
+        dimension_keys_to_clear: list=[],
+        thresholds: dict={
+            500: 300,
+            1500: 600,
+            15000: 1100,
+        }
+):
+    """
+    For monotonic depth profile data, remove data where there are big gaps in
+    the profile. The defined gap sizes are relating to the depth and can be
+    changed using the thresholds - parameter. Default sets to nan if:
+    * depth < 500m and gap > 300m
+    * depth between 500m and 1500m and gap > 600
+    * depth > 1500m and gap > 1100m
+
+    The way it works is that all rows in the gap are removed but one, which is
+    set to NaN.
+    """
+
+    drop_row = 'ROW_DELETE_MARKER'
+    data[drop_row] = False
+
+    prev_index = 0
+    first = True
+    for i, depth in data.loc[data[dimension_key].notnull()][dimension_key].items():
+        if first:
+            first = False
+            prev_index = i
+            continue
+        last_threshold = 0
+        for threshold, gap in thresholds.items():
+            if last_threshold <= depth < threshold :
+                if (depth - data.loc[prev_index, dimension_key]) > gap:
+                    # Set values to nan before / after gap
+                    if prev_index + 1 < i:
+                        data.loc[prev_index+1, dimension_keys_to_clear] = np.nan
+                        data.loc[i-1, dimension_keys_to_clear] = np.nan
+                    # ...and delete rows in gap
+                    if prev_index + 3 < i:
+                        data.loc[prev_index+2:i-2, drop_row] = True
+            last_threshold = threshold
+        prev_index = i
+
+    # Remove rows where ROW_DELETE_MARKER is True
+    data = data.drop(data[data[drop_row]==True].index)
+
+    # deleting the drop_row column
+    del data[drop_row]
+
+    return data
