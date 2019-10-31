2f154a1e29f8e14b1357d6fca0a3fa5ebee08c73
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-10-31 13:32:59 +0100

    (#15) Make relationships between data types

--- util/data_type_dict.py
+++ util/data_type_dict.py
@@ -1,68 +1,52 @@
-#######################################################
-#
-# Get NODC - identifiers for variables and units in
-# data files, primarily used by mockimport.py
-#
-#######################################################
+class DataType:
+    """
+    DataType simply has a name and a BODC identifier. It can be linked to
+    a parent data type, which would be the datatype from the glodap data set.
+    Glodap data types has is_ref_type = True, and should not have parent types.
+    """
+    name = None
+    identifier = None
+    parent_ref_type = None
+    is_ref_type = False
+    def __init__(
+            self,
+            name,
+            identifier = None,
+            parent_ref_type = None,
+            is_ref_type = False,
+    ):
+        self.name = name
+        self.identifier = identifier
+        self.parent_ref_type = parent_ref_type
+        self.is_ref_type = is_ref_type
 
-class MetaDict(type):
-    def __iter__(self):
-        for key in DataTypeDict.conversions:
-            yield key
-    def __getitem__(self, key):
-        return DataTypeDict.getIdentFromVar(key)
+    def set_parent_reference_type(self, parent_ref_type):
+        self.parent_ref_type = parent_ref_type
 
-class DataTypeDict(metaclass=MetaDict):
-    """Thie class is a simple mapping of variable names in glodap, to bodc
+    def __str__(self):
+        return self.name
+
+class DataTypeDict():
+    """This class is a simple mapping of variable names in glodap, to bodc
     vocabularies. It is implemented as an iterable class. Usage:
 
-    >>> DataTypeDict['OXYGEN']
-    'SDN:P01::DOXMZZXX'
+    >>> a = DataTypeDict()
+    >>> a['OXYGEN']
+    <glodap.util.data_type_dict.DataTypeDict.DataType object at 0x7f11d0230d30>
 
-    >>> for key in DataTypeDict:
-            print("{}: {}".format(key, DataTypeDict[key]))
-    G2cfc11: SDN:P01::MDMAP001
-    salinity: SDN:P01::PSALST01
-    G2oxygen: SDN:P01::DOXMZZXX
-    cfc113: SDN:P01::MDMAP003
-    temperature: SDN:P01::TEMPPR01
-    heerr: SDN:P01::HECNMAER
+    >>> for key in a:
+            print("{}".format(a[key]))
+    G2cfc11
+    salinity
+    G2oxygen
     ...
-
 
     Vocabularies are available from the bodc vocabulary search at:
     https://www.bodc.ac.uk/resources/vocabularies/vocabulary_search/
     https://www.bodc.ac.uk/resources/vocabularies/vocabulary_search/P01/
 
     """
-
-    conversions={
-        'TCARBN'       : 'SDN:P01::PCO2XXXX',
-        'ALKALI'       : 'SDN:P01::ALKYZZXX',
-        'OXYGEN'       : 'SDN:P01::DOXMZZXX',
-        'NITRAT'       : 'SDN:P01::NTRAZZXX',
-        'PHSPHT'       : 'SDN:P01::PHOSZZXX',
-        'SILCAT'       : 'SDN:P01::SLCAZZXX',
-        'SALNTY'       : 'SDN:P01::PSALST01', # absolute salinity SDN:P01::ASLTZZ01
-        'CTDSAL'       : 'SDN:P01::PSALST01',
-        'CTDOXY'       : 'SDN:P01::DOXMZZXX',
-        'PH_TOT'       : 'SDN:P01::PHFRSXXX',
-        'THETA'        : 'SDN:P01::POTMCV01',
-        'DOC'          : 'SDN:P01::CORGZZZX',
-        'CFC_11'       : 'SDN:P01::MDMAP001',
-        'CFC_12'       : 'SDN:P01::MDMAP002',
-        'G2tco2'       : 'SDN:P01::PCO2XXXX',
-        'G2talk'       : 'SDN:P01::ALKYZZXX',
-        'G2oxygen'     : 'SDN:P01::DOXMZZXX',
-        'G2nitrate'    : 'SDN:P01::NTRAZZXX',
-        'G2phosphate'  : 'SDN:P01::PHOSZZXX',
-        'G2silicate'   : 'SDN:P01::SLCAZZXX',
-        'G2salinity'   : 'SDN:P01::PSALST01',
-        'G2phts25p0'   : 'SDN:P01::PHTLSX25',
-        'G2theta'      : 'SDN:P01::SIGTEQ01',
-        'G2doc'        : 'SDN:P01::CORGZZZX',
-        'G2cfc11'      : 'SDN:P01::MDMAP001',
-        'G2cfc12'      : 'SDN:P01::MDMAP002',
+    reference_types={
         'tco2'         : 'SDN:P01::PCO2XXXX',
         'talk'         : 'SDN:P01::ALKYZZXX',
         'oxygen'       : 'SDN:P01::DOKGWITX',
@@ -75,9 +59,6 @@ class DataTypeDict(metaclass=MetaDict):
         'doc'          : 'SDN:P01::CORGZZZX',
         'cfc11'        : 'SDN:P01::MDMAP001',
         'cfc12'        : 'SDN:P01::MDMAP002',
-        'CTDTMP'       : 'SDN:P01::TEMPPR01',
-        'NO2+NO3'      : 'SDN:P01::NTRZYYDZ',
-        'NITRIT'       : 'SDN:P01::NTRIZZXX',
         'aou'          : 'SDN:P01::AOUXXXXX',
         'tdn'          : 'SDN:P01::MDMAP013',
         'pcfc11'       : 'SDN:P01::PF11GCTX',
@@ -113,24 +94,70 @@ class DataTypeDict(metaclass=MetaDict):
         'h3err'        : 'SDN:P01::DHE3XXER',
         'phtsinsitutp' : 'SDN:P01::PHFRSXXX',
         'chla'         : 'SDN:P01::CPHLZZXX',
+    }
+
+    exchange_types={
+        'TCARBN'       : 'SDN:P01::PCO2XXXX',
+        'ALKALI'       : 'SDN:P01::ALKYZZXX',
+        'OXYGEN'       : 'SDN:P01::DOXMZZXX',
+        'NITRAT'       : 'SDN:P01::NTRAZZXX',
+        'PHSPHT'       : 'SDN:P01::PHOSZZXX',
+        'SILCAT'       : 'SDN:P01::SLCAZZXX',
+        'SALNTY'       : 'SDN:P01::PSALST01', # absolute salinity SDN:P01::ASLTZZ01
+        'CTDSAL'       : 'SDN:P01::PSALST01',
+        'CTDOXY'       : 'SDN:P01::DOXMZZXX',
+        'PH_TOT'       : 'SDN:P01::PHFRSXXX',
+        'THETA'        : 'SDN:P01::POTMCV01',
+        'DOC'          : 'SDN:P01::CORGZZZX',
+        'CFC_11'       : 'SDN:P01::MDMAP001',
+        'CFC_12'       : 'SDN:P01::MDMAP002',
+        'CTDTMP'       : 'SDN:P01::TEMPPR01',
+        'NO2+NO3'      : 'SDN:P01::NTRZYYDZ',
+        'NITRIT'       : 'SDN:P01::NTRIZZXX',
         'CTDPRS'       : 'SDN:P01::PRESPR01',
         'CFC-11'       : 'SDN:P01::MDMAP001',
         'CFC-12'       : 'SDN:P01::MDMAP002',
         'SF6'          : 'SDN:P01::PSF6XXXX',
         'CHLORA'       : 'SDN:P01::CPHLZZXX',
         'PHAEO'        : 'SDN:P01::PTAXZZXX',
-
-        # Units
-        'DBAR'       : 'SDN:P06::UPDB',
-        'ITS-90'     : 'SDN:P06::UPAA',
-        'PSS-78'     : 'SDN:S06::S0600083',
-        'UMOL/KG'    : 'SDN:P06::KGUM',
-        'PMOL/KG'    : 'SDN:P06::KGPM',
-        'FMOL/KG'    : 'SDN:P06::FGKG',
     }
+    typelist = {}
 
 
 
-    @classmethod
-    def getIdentFromVar(cls, var_name):
-        return cls.conversions.get(var_name) or ""
+    def __init__(self):
+        for name in self.reference_types:
+            self.typelist[name] = DataType(
+                name,
+                is_ref_type = True,
+                identifier = self.reference_types[name]
+            )
+        for name in self.exchange_types:
+            self.typelist[name] = DataType(
+                name,
+                parent_ref_type = self.typelist.get(
+                    self.get_ref_type(self.exchange_types[name])
+                ),
+                identifier = self.exchange_types[name],
+            )
+
+    def getIdentifier(self, var_name):
+        return self.typelist.get(var_name).identifier
+
+    def get_ref_type(self, identifier):
+        for k,v in self.reference_types.items():
+            if v == identifier:
+                return k
+        return None
+
+    def __iter__(self):
+        return iter(self.typelist)
+
+    def __getitem__(self, key):
+        return self.typelist[key]
+
+    def __len__(self):
+        return len(self.typelist)
+
+    def items(self):
+        return self.typelist.items()
